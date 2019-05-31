ac8c4859ad0ea6e3e9a35489587afe0b3b963a23
mjfalahee <mjfalahee@gmail.com>
2019-05-31 08:39:11 -0700

    Installed dependencies - Express, Nodemon, Helmet -- and started to configure the server

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const helmet = require('helmet');
+// const postRouter = require('./posts/postRouter');
+// const userRouter = require('./users/userRouter');
+
+const server = express();
+
+server.use(
+  express.json(),
+  helmet(), 
+  logger
+  );
+
+server.get('/', (req, res) => {
+  res.send(`<h2>Sprint Challenge: Web API</h2>`)
+});
+
+// server.use('/api/posts', postRouter);
+// server.use('/api/users', userRouter);
+
+
+//custom middleware
+
+
+function logger(req, res, next) {
+const currentTime = new Date();
+const clockTime = "At: " + currentTime.getHours() + ":" + currentTime.getMinutes() + ":" + currentTime.getSeconds();
+
+console.log(`A ${req.method} request to '${req.url}'`);
+console.log(`${clockTime}`)
+next();
+};
+
+module.exports = server;
